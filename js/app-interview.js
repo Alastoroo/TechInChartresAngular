3e5a7ce50dfd658fca565bfc396e42f448706ba7
kvn29 <toun29@gmail.com>
2016-10-27 11:55:00 +0200

    Interview Work In Progress

--- js/app-interview.js
+++ js/app-interview.js
@@ -1,7 +1,23 @@
+$.fn.animateRotate = function(angle, duration, easing, complete) {
+  var args = $.speed(duration, easing, complete);
+  var step = args.step;
+  return this.each(function(i, e) {
+    args.complete = $.proxy(args.complete, e);
+    args.step = function(now) {
+      $.style(e, 'transform', 'rotate(' + now + 'deg)');
+      if (step) return step.apply(e, arguments);
+    };
+
+    $({deg: 0}).animate({deg: angle}, args);
+  });
+};
+
+
 (function() {
   var countInterview = undefined;
 
   var app = angular.module('TechInChartres', []);
+  app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
 
   app.controller('mainController', function($scope, $http) {
     $scope.data = undefined;
@@ -40,6 +56,14 @@
     $http.get('js/interviewExample.json').success(function (data) {
       $scope.data = data;
       countInterview = data.interviews.length; // Ici on attribut le nombre d'interview a cette variable
+
+      // ici on parse les Questions, en se servant de la balise [Q]
+      for(var interview in data.interviews) {
+        var questionsReponses = data.interviews[interview].content;
+        questionsReponses = questionsReponses.replace( /\[Q\](.+?)\[\/Q]/gi, "<h1 class='interviewQuestion'>$1</h1>" );
+        data.interviews[interview].content = questionsReponses;
+      }
+
     });
   });
 
@@ -73,19 +97,67 @@
       // Donc on montre le bloc qui le dit
       if(countInterview === $('div.ng-hide').length) {
         $('.noresult').removeClass('hideBloc');
-        console.log('????')
       }
       else {
         $('.noresult').addClass('hideBloc');
-        console.log('??')
       }
     });
 
-    $('.arrows').click(function() {
-      console.log('aa');
-    });
-    console.log($('.arrows').length);
+  });
+  // On écoute le moindre click. On ne peut utiliser la fonctionc click puisque les éléments
+  // ne sont pas encore présents
+  $(document).on("click", ".arrow", function(){
+    const durationAnimation = 200;
 
+    var $currentInterview = $(this).parent().parent().parent().parent().parent().parent().parent().parent();
+    var $currentArrow = $(this);
+
+    if($('.arrowActive').length > 0) {
+      // Si on clique sur l'élément qui est ouvert
+      if($(this).hasClass('arrowActive')) {
+        console.log('aa');
+        $(this).removeClass('arrowActive').animateRotate(0, {
+          duration: durationAnimation,
+          easing: 'linear',
+          complete: function() {
+            $currentInterview.css('height', '180px');
+          },
+          step: function() {}
+        });
+      }
+      else {
+        $('.arrow').removeClass('arrowActive').animateRotate(0, {
+          duration: durationAnimation,
+          easing: 'linear',
+          complete: function() {
+
+            $currentArrow.addClass('arrowActive').animateRotate(180, {
+              duration: durationAnimation,
+              easing: 'linear',
+              complete: function () {
+                $('.interview').css('height', '180px');
+                $currentInterview.css('height', 'inherit');
+              },
+              step: function () {}
+            });
+
+          },
+          step: function() {}
+        });
+      }
+    }
+    else {
+      // Tout est fermé
+
+      $(this).addClass('arrowActive').animateRotate(180, {
+        duration: durationAnimation,
+        easing: 'linear',
+        complete: function () {
+          $currentInterview.css('height', 'inherit');
+        },
+        step: function () {}
+      });
+    }
   });
 
 })();
